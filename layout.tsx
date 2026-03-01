f7241122c0179eb0f23227831990f49554271991
hassaniazi1204 <166181429+hassaniazi1204@users.noreply.github.com>
2026-03-01 06:57:17 +0500

    Update layout.tsx

--- layout.tsx
+++ layout.tsx
@@ -2,8 +2,8 @@ import type { Metadata } from "next";
 import "./globals.css";
 
 export const metadata: Metadata = {
-  title: "Ritual Quiz Platform",
-  description: "Test your knowledge about Ritual - The world's first sovereign execution layer for AI",
+  title: "SiggyDrop",
+  description: "A community run platform to promote Ritual",
 };
 
 export default function RootLayout({
